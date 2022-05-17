d9840a01812764811800aa8c598e2011255888b2
tonlls <tonlls1999@gmail.com>
2022-05-17 15:41:48 +0200

    requirements

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from Models import Event, Group, User
-from fastapi import FastAPI, HTTPException
-from starlette.responses import Response
-
-from app.db.models import UserAnswer
-from app.api import api
+from fastapi import FastAPI
 
 
 
