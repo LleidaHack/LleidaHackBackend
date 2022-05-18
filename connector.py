e575e81601a7985df93917ac44a41345f05da94a
tonlls <tonlls1999@gmail.com>
2022-05-18 13:03:52 +0200

    many things

---
+++ connector.py
@@ -0,0 +1,11 @@
+class database_connector():
+    def __init__(self) -> None:
+        pass
+    def connect(self, url:str) -> None:
+        pass
+
+class postgres(database_connector):
+    def __init__(self) -> None:
+        pass
+    def connect(self, url:str) -> None:
+        pass
