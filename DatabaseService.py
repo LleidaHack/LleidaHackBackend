0a4f20eeb91721738f4b8d0d4d292e301955fd33
tonlls <tonlls1999@gmail.com>
2022-05-25 13:55:01 +0200

    littlle fixes

--- DatabaseService.py
+++ DatabaseService.py
@@ -11,9 +11,9 @@ class DatabaseService():
     def getUser(self, id: int) -> list:
         return self.connector.execute_query("SELECT * FROM users WHERE user_id = %s" % id)
     def addUser(self, user: User):
-        return self.connector.execute_query("INSERT INTO users (name, password, email, nickname, birthdate, phone_number, food_restrictions, shirt_size) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (user.name, user.password, user.email, user.nickname, user.birthdate, user.phone_number, user.food_restrictions, user.shirt_size))
+        return self.connector.execute_query("INSERT INTO users (name, password, email, nickname, birthdate, telephone, food_restrictions, shirt_size) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (user.name, user.password, user.email, user.nickname, user.birthdate, user.telephone, user.food_restrictions, user.shirt_size))
     def updateUser(self, user: User):
-        return self.connector.execute_query("UPDATE users SET name = %s, password = %s, email = %s, nickname = %s, birthdate = %s, phone_number = %s, food_restrictions = %s, shirt_size = %s WHERE id = %s", (user.name, user.password, user.email, user.nickname, user.birthdate, user.phone_number, user.food_restrictions, user.shirt_size, user.id))
+        return self.connector.execute_query("UPDATE users SET name = %s, password = %s, email = %s, nickname = %s, birthdate = %s, telephone = %s, food_restrictions = %s, shirt_size = %s WHERE id = %s", (user.name, user.password, user.email, user.nickname, user.birthdate, user.telephone, user.food_restrictions, user.shirt_size, user.id))
     def deleteUser(self, id: int):
         return self.connector.execute_query("DELETE FROM users WHERE id = %s", (id))
     def banUser(self, id: int):
