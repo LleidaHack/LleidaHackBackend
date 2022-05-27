5654b2ebe9d76bf8979c1aa02b7daa84de2b97c1
tonlls <tonlls1999@gmail.com>
2022-05-27 14:26:14 +0200

    ORM test new

--- new/main.py
+++ new/main.py
@@ -1,3 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from .schemas import CreateJobRequest
+from sqlalchemy.orm import Session
+from .database import get_db
+from .models import Job
 
 app= FastAPI()
+@app.post("/")
+def create_job(job: CreateJobRequest, db: Session = Depends(get_db)):
+    new_job = Job(title=job.title, description=job.description)
+    db.add(new_job)
+    db.commit()
+    # db.refresh(new_job)
+    return {"success": True, "created_id": new_job.id}
