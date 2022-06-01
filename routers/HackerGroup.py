d60a5462a26856fba75916b670481915ae25ea70
tonlls <tonlls1999@gmail.com>
2022-06-01 11:53:48 +0200

    add all routes

--- routers/HackerGroup.py
+++ routers/HackerGroup.py
@@ -10,21 +10,21 @@ from fastapi import Depends, Response, APIRouter
 
 router = APIRouter(
     prefix="/hackergroup",
-    tags=["HackerGroup"],
+    tags=["Hacker Group"],
     # dependencies=[Depends(get_db)],
     # dependencies=[Depends(get_token_header)],
     # responses={404: {"description": "Not found"}},
 )
 
-@router.get("/all", tags=["Hacker Group"])
+@router.get("/all")
 async def get_hacker_groups(db: Session = Depends(get_db)):
     return db.query(ModelHackerGroup).all()
 
-@router.get("/{groupId}", tags=["Hacker Group"])
+@router.get("/{groupId}")
 async def get_hacker_group(groupId: int, response: Response, db: Session = Depends(get_db)):
     return db.query(ModelHackerGroup).filter(ModelHackerGroup.id == groupId).first()
 
-@router.post("/", tags=["Hacker Group"])
+@router.post("/")
 async def add_hacker_group(payload:SchemaHackerGroup, response: Response, db: Session = Depends(get_db)):
     new_hacker_group = ModelHackerGroup(name=payload.name,
                                         description=payload.description,
@@ -33,7 +33,7 @@ async def add_hacker_group(payload:SchemaHackerGroup, response: Response, db: Se
     db.commit()
     return {"success": True, "created_id": new_hacker_group.id}
 
-@router.put("/{groupId}", tags=["Hacker Group"])
+@router.put("/{groupId}")
 async def update_hacker_group(groupId: int, payload: SchemaHackerGroup, response: Response, db: Session = Depends(get_db)):
     hacker_group = db.query(ModelHackerGroup).filter(ModelHackerGroup.id == groupId).first()
     hacker_group.name = payload.name
@@ -41,7 +41,7 @@ async def update_hacker_group(groupId: int, payload: SchemaHackerGroup, response
     db.commit()
 
 
-@router.delete("/{groupId}", tags=["Hacker Group"])
+@router.delete("/{groupId}")
 async def delete_hacker_group(groupId:int, response: Response, db: Session = Depends(get_db)):
     # result = VerifyToken(token.credentials).verify()
     # if result.get("status"):
@@ -50,11 +50,11 @@ async def delete_hacker_group(groupId:int, response: Response, db: Session = Dep
     db.query(ModelHackerGroup).filter(ModelHackerGroup.id == groupId).delete()
     return {"success": True}
 
-@router.get("/{groupId}/members", tags=["Hacker Group"])
+@router.get("/{groupId}/members")
 async def get_hacker_group_members(groupId: int, response: Response, db: Session = Depends(get_db)):
     return db.query(ModelHackerGroup).filter(ModelHackerGroup.id == groupId).all().members
 
-@router.post("/{groupId}/add/{hackerId}", tags=["Hacker Group"])
+@router.post("/{groupId}/add/{hackerId}")
 async def add_hacker_to_group(groupId: int, hackerId: int, response: Response, db: Session = Depends(get_db)):
     hacker_group = db.query(ModelHackerGroup).filter(ModelHackerGroup.id == groupId).first()
     hacker = db.query(ModelHacker).filter(ModelHacker.id == hackerId).first()
