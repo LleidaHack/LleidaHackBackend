95cfeb3515ad911ee1e40b27054ce338134e1771
tonlls <tonlls1999@gmail.com>
2022-06-02 12:09:00 +0200

    fix

--- database.py
+++ database.py
@@ -8,8 +8,8 @@ from sqlalchemy_utils import database_exists, create_database
 load_dotenv('.env')
 SQLALCHEMY_DATABASE_URL =  os.environ.get("DATABASE_URL")
 engine = create_engine ( SQLALCHEMY_DATABASE_URL )
-if not database_exists(engine.url):
-    create_database(engine.url)
+# if not database_exists(engine.url):
+    # create_database(engine.url)
 SessionLocal = sessionmaker ( autocommit = False , autoflush = False , bind = engine )
 Base = declarative_base ( )
 
