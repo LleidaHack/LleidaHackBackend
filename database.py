59cc944bb6be7d212b039fe60e868f750a392dc9
tonlls <tonlls1999@gmail.com>
2022-06-06 17:36:58 +0200

    lots of things and improvements!!!!!!!!!!!!!!!!!

--- database.py
+++ database.py
@@ -2,12 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-from sqlalchemy_utils import database_exists, create_database
+# from sqlalchemy_utils import database_exists, create_database
+from config import Configuration
 
-load_dotenv('.env')
-SQLALCHEMY_DATABASE_URL =  os.environ.get("DATABASE_URL")
-engine = create_engine ( SQLALCHEMY_DATABASE_URL )
+engine = create_engine(Configuration.get("POSTGRESQL","DATABASE_URL"))
 # if not database_exists(engine.url):
     # create_database(engine.url)
 SessionLocal = sessionmaker ( autocommit = False , autoflush = False , bind = engine )
