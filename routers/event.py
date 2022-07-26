75bc3354c90a8a27c3a6e5dc4f76635813e49f44
Ton <47422572+tonlls@users.noreply.github.com>
2022-07-26 17:25:28 +0200

    fix async

--- routers/event.py
+++ routers/event.py
@@ -27,71 +27,69 @@ router = APIRouter(
 )
 
 @router.get("/")
-def get_events(db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
+async def get_events(db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
     return event_service.get_all(db)
 
 @router.get("/{id}")
-def get_event(id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
+async def get_event(id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
     return event_service.get_event(id, db)
 
 @router.post("/")
-def create_event(event: SchemaEvent, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
+async def create_event(event: SchemaEvent, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
     new_event = await event_service.add_event(event, db)
     return {'success': True, 'event_id': new_event.id}
 
 @router.put("/{id}")
-def update_event(id: int, event: SchemaEvent, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
+async def update_event(id: int, event: SchemaEvent, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
     new_event = await event_service.update_event(id, event, db)
     return {'success': True, 'event_id': new_event.id}
 
 @router.delete("/{id}")
-def delete_event(id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
+async def delete_event(id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
     event = await event_service.delete_event(id, db)
     return {'success': True, 'event_id': event.id}
 
 @router.get("/{id}/participants")
-def get_event_participants(id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
+async def get_event_participants(id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
     event = event_service.get_event(id, db)
     return event.participants
 
 @router.get("/{id}/sponsors")
-def get_event_sponsors(id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
+async def get_event_sponsors(id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
     event = event_service.get_event(id, db)
     return event.sponsors
 
 @router.get("/{id}/groups")
-def get_event_groups(id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
+async def get_event_groups(id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
     event = event_service.get_event(id, db)
     return {'success': True, 'groups': event.groups}
 
 @router.put("/{id}/participants/{hacker_id}")
-def add_event_participant(id: int, hacker_id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
+async def add_event_participant(id: int, hacker_id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
     event = await event_service.add_hacker(id, hacker_id, db)
     return {'success': True, 'event_id': event.id}
 
 @router.delete("/{id}/participants/{hacker_id}")
-def remove_event_participant(id: int, hacker_id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
+async def remove_event_participant(id: int, hacker_id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
     event = await event_service.remove_hacker(id, hacker_id, db)
     return {'success': True, 'event_id': event.id}
 
 @router.put("/{id}/sponsors/{company_id}")
-def add_event_sponsor(id: int, company_id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
+async def add_event_sponsor(id: int, company_id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
     event = await event_service.add_company(id, company_id, db)
     return {'success': True, 'event_id': event.id}
 
 @router.delete("/{id}/sponsors/{company_id}")
-def remove_event_sponsor(id: int, company_id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
+async def remove_event_sponsor(id: int, company_id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
     event = await event_service.remove_company(id, company_id, db)
     return {'success': True, 'event_id': event.id}
 
 @router.put("/{id}/group/{group_id}")
-def add_event_group(id: int, group_id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
+async def add_event_group(id: int, group_id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
     event = await event_service.add_group(id, group_id, db)
     return {'success': True, 'event_id': event.id}
 
 @router.delete("/{id}/group/{group_id}")
-def remove_event_group(id: int, group_id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
+async def remove_event_group(id: int, group_id: int, db: Session = Depends(get_db), tokem: str = Depends(oauth_schema)):
     event = await event_service.remove_group(id, group_id, db)
     return {'success': True, 'event_id': event.id}
-
-
