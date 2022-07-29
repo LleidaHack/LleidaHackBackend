4a49a8d302ef9778b4ab80483b19de60113df6ca
tonlls <tonlls1999@gmail.com>
2022-07-29 20:08:00 +0200

    add image id to all users

---
+++ alembic/versions/bb840e8a133f_image_id_fix.py
@@ -0,0 +1,24 @@
+"""image_id fix
+
+Revision ID: bb840e8a133f
+Revises: 12a91ff3996b
+Create Date: 2022-07-29 20:03:36.512686
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bb840e8a133f'
+down_revision = '12a91ff3996b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_column('llhk_user', sa.Column('image_id', sa.String(), nullable=True))
+
+
+def downgrade():
+    pass
