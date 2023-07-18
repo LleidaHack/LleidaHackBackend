113de27e5de290332e8b96db2d8909b2fd042a89
tonlls <tonlls1999@gmail.com>
2023-07-18 15:42:56 +0200

    fixed

--- services/mail.py
+++ services/mail.py
@@ -9,18 +9,18 @@ class EmailSchema(BaseModel):
     email: List[EmailStr]
 
 
-conf = ConnectionConfig(
-    MAIL_USERNAME = Configuration.get('MAIL', 'MAIL_USERNAME'),
-    MAIL_PASSWORD = Configuration.get('MAIL', 'MAIL_PASSWORD'),
-    MAIL_FROM = Configuration.get('MAIL', 'MAIL_FROM'),
-    MAIL_PORT = int(Configuration.get('MAIL', 'MAIL_PORT')),
-    MAIL_SERVER = Configuration.get('MAIL', 'MAIL_SERVER'),
-    MAIL_FROM_NAME = Configuration.get('MAIL', 'MAIL_FROM_NAME'),
-    MAIL_TLS = True,
-    MAIL_SSL = False,
-    USE_CREDENTIALS = True,
-    VALIDATE_CERTS = True
-)
+# conf = ConnectionConfig(
+#     MAIL_USERNAME = Configuration.get('MAIL', 'MAIL_USERNAME'),
+#     MAIL_PASSWORD = Configuration.get('MAIL', 'MAIL_PASSWORD'),
+#     MAIL_FROM = Configuration.get('MAIL', 'MAIL_FROM'),
+#     MAIL_PORT = int(Configuration.get('MAIL', 'MAIL_PORT')),
+#     MAIL_SERVER = Configuration.get('MAIL', 'MAIL_SERVER'),
+#     MAIL_FROM_NAME = Configuration.get('MAIL', 'MAIL_FROM_NAME'),
+#     MAIL_TLS = True,
+#     MAIL_SSL = False,
+#     USE_CREDENTIALS = True,
+#     VALIDATE_CERTS = True
+# )
 
 app = FastAPI()
 
@@ -29,14 +29,14 @@ html = """<p>Hi this test mail, thanks for using Fastapi-mail</p> """
 
 
 async def simple_send(email: EmailSchema) -> JSONResponse:
+    pass
+    # message = MessageSchema(
+    #     subject="Fastapi-Mail module",
+    #     recipients=email.dict().get("email"),  # List of recipients, as many as you can pass 
+    #     body=html,
+    #     subtype="html"
+    # )
 
-    message = MessageSchema(
-        subject="Fastapi-Mail module",
-        recipients=email.dict().get("email"),  # List of recipients, as many as you can pass 
-        body=html,
-        subtype="html"
-    )
-
-    fm = FastMail(conf)
-    await fm.send_message(message)
-    return JSONResponse(status_code=200, content={"message": "email has been sent"})
+    # fm = FastMail(conf)
+    # await fm.send_message(message)
+    # return JSONResponse(status_code=200, content={"message": "email has been sent"})
