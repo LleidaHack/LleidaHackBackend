846eb7ec15fcf49021badc018c8ba6a8c8bceec9
tonlls <tonlls1999@gmail.com>
2023-07-29 19:52:10 +0200

    fix company

--- routers/company.py
+++ routers/company.py
@@ -16,11 +16,11 @@ router = APIRouter(
 
 @router.get("/all")
 async def get_companies(db: Session = Depends(get_db), str = Depends(oauth_schema)):
-    return company_service.get_all(db)
+    return await company_service.get_all(db)
 
 @router.get("/{companyId}")
 async def get_company(companyId: int, response: Response, db: Session = Depends(get_db), str = Depends(oauth_schema)):
-    return company_service.get_company(db, companyId)
+    return await company_service.get_company(db, companyId)
 
 @router.post("/")
 async def add_company(payload:SchemaCompany, response: Response, db: Session = Depends(get_db), str = Depends(oauth_schema)):
