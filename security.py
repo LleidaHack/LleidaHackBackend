b1503aaa719cdd25a8df3193e429ade58cb3abea
Ton <47422572+tonlls@users.noreply.github.com>
2023-08-16 01:06:52 +0200

    Update security.py

--- security.py
+++ security.py
@@ -91,7 +91,7 @@ def create_confirmation_token(email: str):
 
 
 def get_data_from_token(token: str = Depends(oauth2_scheme)):
-    d = TokenData()
+    d = TokenData(user_id=0, type='')
     if is_service_token(token):
         d.is_admin = True
         d.is_service = True
