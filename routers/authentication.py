878440504fcbcb62e9f6af44fc40489eaa403e4b
Ton <47422572+tonlls@users.noreply.github.com>
2023-08-16 00:29:21 +0200

    Update authentication.py

--- routers/authentication.py
+++ routers/authentication.py
@@ -8,7 +8,7 @@ from database import get_db
 from fastapi import Depends, Request, HTTPException, status
 from sqlalchemy.orm import Session
 from fastapi.security import HTTPBasicCredentials
-from security import ACCESS_TOKEN_EXPIRE_MINUTES, authenticate_user, create_access_token
+from security import ACCESS_TOKEN_EXPIRE_MINUTES, authenticate_user, create_access_token, sec
 
 router = APIRouter(
     prefix="",
