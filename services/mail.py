628fbf5391d2b2b9f3a94b5a076720f1e17e3fee
Quiracle <jsalattorres@gmail.com>
2023-11-04 18:41:12 +0100

    Update mail.py

--- services/mail.py
+++ services/mail.py
@@ -42,9 +42,7 @@ def send_bulk_mails(lst: List):
 def send_email(user, body: str, subject: str, queue: bool = False):
     mail = user
     if not queue:
-        if type(user) is ModelUser:
-            return {"success": "wtf"}
-            mail = user.email
+        mail = user.email
         mail_queue_service.send_email(mail, body, subject)
     else:
         db = db_get()
