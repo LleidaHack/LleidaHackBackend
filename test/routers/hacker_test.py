0a591ad09cf475d79f3d7b72e6254f973952a600
euhean <ehernandeza02@gmail.com>
2024-02-27 22:44:05 +0100

    Add existing file

---
+++ test/routers/hacker_test.py
@@ -0,0 +1,60 @@
+from unittest.mock import patch
+from fastapi.testclient import TestClient
+from schemas.Hacker import Hacker as SchemaHacker
+from main import app
+import random
+import string
+import base64
+
+client = TestClient(app)
+
+
+def random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+def random_password(length):
+    letters = string.ascii_letters + string.digits
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str[0].upper() + result_str[1:]
+
+
+async def mock_add_hacker():
+    return {"id": 1}
+
+
+def mock_create_all_tokens():
+    return "fake_access_token", "fake_refresh_token"
+
+
+@patch("services.hacker.add_hacker", new=mock_add_hacker)
+@patch("security.create_all_tokens", new=mock_create_all_tokens)
+def test_hacker_signup():
+    payload = {
+        SchemaHacker(name="test",
+                     nickname=random_string(10),
+                     password=random_password(10),
+                     birthdate="2000-01-01",
+                     food_restrictions="None",
+                     email=random_string(5) + "@test.com",
+                     telephone="".join(
+                         [str(random.randint(0, 9)) for _ in range(10)]),
+                     address="test",
+                     shirt_size="M",
+                     image=base64.b64encode(b'test').decode('utf-8'),
+                     is_image_url=False,
+                     recive_mails=True,
+                     github="string",
+                     linkedin="string"
+                     )
+    }
+    response = client.post("/signup", json=payload)
+    assert response.status_code == 200
+    expected_response_body = {
+        "success": True,
+        "user_id": 1,
+        "access_token": "fake_access_token",
+        "refresh_token": "fake_refresh_token"
+    }
+    assert response.json() == expected_response_body
