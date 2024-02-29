7d058362365039b0880ef79f99bd7129151788ef
euhean <ehernandeza02@gmail.com>
2024-02-29 22:16:36 +0100

    Més testos de hacker endpoints

--- test/routers/hacker_test.py
+++ test/routers/hacker_test.py
@@ -1,7 +1,6 @@
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from fastapi.testclient import TestClient
 from schemas.Hacker import Hacker as SchemaHacker
-from database import db_get
 from main import app
 import random
 import string
@@ -75,11 +74,73 @@ def mock_jwt_bearer():
 def test_get_hackers():
     response = client.get("/all")
     assert response.status_code == 200
-    expected_response_body = [{
-        "id": 1,
-        "name": "Hacker1"
-    }, {
-        "id": 2,
-        "name": "Hacker2"
-    }]
+    expected_response_body = [{"id": 1, "name": "Hacker1"}, {"id": 2, "name": "Hacker2"}]
+    assert response.json() == expected_response_body
+
+
+async def mock_get_hacker(hacker_id):
+    return {"id": hacker_id, "name": "TestHacker"}
+
+
+def mock_jwt_bearer():
+    return "fake_token"
+
+
+@patch("services.hacker.get_hacker", new=mock_get_hacker)
+@patch("routers.hacker.get_hacker.JWTBearer", new=mock_jwt_bearer)
+def test_get_hacker():
+    response = client.get("/1")
+    assert response.status_code == 200
+    expected_response_body = {"id": 1, "name": "TestHacker"}
+    assert response.json() == expected_response_body
+
+
+async def mock_update_hacker(hacker_id):
+    return MagicMock(hacker_id), True
+
+
+def mock_jwt_bearer():
+    return "fake_token"
+
+
+@patch("services.hacker.update_hacker", new=mock_update_hacker)
+@patch("routers.hacker.update_hacker.JWTBearer", new=mock_jwt_bearer)
+def test_update_hacker():
+    response = client.put("/1", json={"name": "UpdatedHacker"})
+    assert response.status_code == 200
+    expected_response_body = {"success": True, "updated_id": 1, "updated": True}
+    assert response.json() == expected_response_body
+
+
+async def mock_ban_hacker(user_id):
+    return MagicMock(user_id)
+
+
+def mock_jwt_bearer():
+    return "fake_token"
+
+
+@patch("service.hacker.ban_hacker", new=mock_ban_hacker)
+@patch("routers.hacker.ban_hacker.JWTBearer", new=mock_jwt_bearer)
+def test_ban_hacker():
+    response = client.post("/1/ban")
+    assert response.status_code == 200
+    expected_response_body = {"success": True, "banned_id": 1}
+    assert response.json() == expected_response_body
+
+
+async def mock_unban_hacker(user_id):
+    return MagicMock(user_id)
+
+
+def mock_jwt_bearer():
+    return "fake_token"
+
+
+@patch("service.hacker.unban_hacker", new=mock_unban_hacker)
+@patch("routers.hacker.unban_hacker.JWTBearer", new=mock_jwt_bearer)
+def test_unban_hacker():
+    response = client.post("/1/unban")
+    assert response.status_code == 200
+    expected_response_body = {"success": True, "unbanned_id": 1}
     assert response.json() == expected_response_body
