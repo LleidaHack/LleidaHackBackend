8c5c28ca23aac9b7af9ee8987e01809dfb6417ec
euhean <ehernandeza02@gmail.com>
2024-03-26 18:58:16 +0100

    més testos de endpoints

--- test/routers/hacker_test.py
+++ test/routers/hacker_test.py
@@ -50,7 +50,12 @@ schema_hacker = {
     "is_image_url": "False",
     "receive_mails": "True",
     "github": "test",
-    "linkedin": "test"
+    "linkedin": "test",
+    "studies": "string",
+    "study_center": "string",
+    "location": "string",
+    "how_did_you_meet_us": "string",
+    "cv": "string"
 }
 
 mock_access_token = "fake_access_token"
@@ -64,7 +69,6 @@ def test_signup(mock_send_email, mock_create_tokens):
     fake_db = MagicMock(TestingSessionLocal)
     response = client.post("/hacker/signup", json=schema_hacker)
     assert response.status_code == 200
-
     expected_response_body = {
         "success": True,
         "user_id": 1,
@@ -72,7 +76,6 @@ def test_signup(mock_send_email, mock_create_tokens):
         "refresh_token": mock_refresh_token
     }
     assert response.json() == expected_response_body
-
     new_hacker = services.hacker.get_hacker(1, TestingSessionLocal, TokenData)
     mock_create_tokens.assert_called_once_with(new_hacker,
                                                fake_db,
@@ -80,4 +83,36 @@ def test_signup(mock_send_email, mock_create_tokens):
     mock_send_email.assert_called_once_with(new_hacker)
 
 
-#@patch("services.hacker.get_all", return_value="test")
+def test_get_hackers():
+    response = client.get("/hacker/all")
+    assert response.status_code == 200
+    assert response.json() == "test"
+
+
+def test_get_hacker():
+    response = client.get("/hacker/1")
+    assert response.status_code == 200
+    assert response.json() == "test"
+
+
+def test_update_hacker():
+    response = client.post("/hacker/1", json=schema_hacker)
+    assert response.status_code == 200
+    updated = services.hacker.get_hacker(1, TestingSessionLocal, TokenData)
+    expected_response_body = {
+        "success": True,
+        "updated_id": updated.id,
+        "updated": updated
+    }
+    assert response.json() == expected_response_body
+
+
+def test_ban_hacker():
+    response = client.post("/hacker/1/ban")
+    assert response.status_code == 200
+    hacker = services.hacker.get_hacker(1, TestingSessionLocal, TokenData)
+    expected_response_body = {
+        "success": True,
+        "banned_id": hacker.id
+    }
+    assert response.json() == expected_response_body
