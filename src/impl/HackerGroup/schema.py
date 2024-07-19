dd1161e445539103bc3860d24c1f3e46ddf2a7b9
Jonàs Salat <57363628+Quiracle@users.noreply.github.com>
2024-07-19 10:50:44 +0200

    fix: ha petat els ultims 4 fixes

--- src/impl/HackerGroup/schema.py
+++ src/impl/HackerGroup/schema.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from pydantic import ConfigDict
 
@@ -18,7 +18,7 @@ class HackerGroupGet(BaseSchema):
     description: str
     leader_id: int
     event_id: int
-    members
+    members: List[object]
 
 
 class HackerGroupGetAll(HackerGroupGet):
