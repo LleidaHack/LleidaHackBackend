08f53844db34af520e89caf9e9051bef254358df
elver <joelros2003@gmail.com>
2024-08-01 18:17:44 +0200

    get and get all service for article and type

--- src/impl/Article/service.py
+++ src/impl/Article/service.py
@@ -1,5 +1,20 @@
+from fastapi_sqlalchemy import db
+
+from src.error.NotFoundException import NotFoundException
+from src.impl.Article.model import Article as ModelArticle
+
 from src.utils.Base.BaseService import BaseService
 
 
 class ArticleService(BaseService):
-    pass
+    name = 'article_service'
+    
+    def get_all(self):
+        return db.session.query(ModelArticle).all()
+
+    def get_by_id(self, articleId: int):
+        article = db.session.query(ModelArticle).filter(
+            ModelArticle.id == articleId).first()
+        if article is None:
+            raise NotFoundException('article not found')
+        return article
--- src/impl/ArticleType/service.py
+++ src/impl/ArticleType/service.py
@@ -1,5 +1,19 @@
+from fastapi_sqlalchemy import db
+
+from src.error.NotFoundException import NotFoundException
+from src.impl.ArticleType.model import ArticleType as ModelArticleType
 from src.utils.Base.BaseService import BaseService
 
 
 class ArticleTypeService(BaseService):
-    pass
+    name = 'article_type_service'
+    
+    def get_all(self):
+        return db.session.query(ModelArticleType).all()
+
+    def get_by_id(self, articleTypeId: int):
+        articleType = db.session.query(ModelArticleType).filter(
+            ModelArticleType.id == articleTypeId).first()
+        if articleType is None:
+            raise NotFoundException('article type not found')
+        return articleType
