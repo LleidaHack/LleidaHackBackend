ff21629db2c95c932895c5f94a01ea0641af9f72
elver5041 <joelros2003@gmail.com>
2024-09-26 00:31:35 +0200

    get->post

--- src/impl/Authentication/router_v1.py
+++ src/impl/Authentication/router_v1.py
@@ -29,7 +29,8 @@ def reset_password(email: str):
 
 @router.post("/confirm-reset-password")
 def confirm_reset_password(token: str, password: str):
-    return auth_service.confirm_reset_password(token, password)
+    return auth_service.confirm_reset_password(BaseToken.get_data(token),
+                                               password)
 
 
 @router.post("/refresh-token")
@@ -62,6 +63,6 @@ def check_token(token: BaseToken = Depends(JWTBearer())):
     return {"success": True}
 
 
-@router.get("/contact")
+@router.post("/contact")
 def contact(name: str, title: str, email: str, message: str):
     return auth_service.contact(name, title, email, message)
